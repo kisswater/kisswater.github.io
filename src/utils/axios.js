a723d3dcaf5c1155c58dfc8cff5159fa9f1642e4
dotryme <578595193@qq.com>
2019-08-30 14:14:00 +0800

    FIX

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import store from '../store'
 axios.interceptors.request.use(function (config) {
   // Do something before request is sent
-  if (/get/i.test(config.method)) {
+  if (/get/i.test(config.method) && /users|issues|labels/i.test(config.url)) {
     if (store.state.account.apiAccessToken) {
       config.params = config.params || {}
       config.headers['Authorization'] = `token ${store.state.account.apiAccessToken.join('')}`
